5a9875276f91313dd355140585a8df3e0bc2c250
zymeth <Zymeth@users.noreply.github.com>
2021-01-13 15:50:44 +0100

    menu + anim equalizer

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -5,29 +5,72 @@
         <img :src="this.$store.state.options.menu.logo" :alt="this.$store.state.options.generales.seo.title" />
       </NuxtLink>
     </div>
-    <nav class="main-menu" >
-      <ul class="menu-list">
-        <li v-for="item in this.$store.state.options.menu.menuList"><NuxtLink :to="item.url">{{ item.nomdepage }}</NuxtLink></li>
-      </ul>
-      <ul class="rs-list">
-        <li v-for="item in this.$store.state.options.menu.rs"><NuxtLink :to="item.value"><i :class=" item.label "/></NuxtLink></li>
-      </ul>
-    </nav>
+    <div class="main-menu" :class="{active: MenuIsActive}" >
+      <div id="menu-select"  @click="openMenu">
+          <p></p>
+          <span :style="bar1"></span>
+          <span :style="bar2"></span>
+          <span :style="bar3"></span>
+      </div>
+      <nav class="nav-list">
+          <ul class="menu-list">
+            <li v-for="item in this.$store.state.options.menu.menuList"><NuxtLink :to="item.url">{{ item.nomdepage }}</NuxtLink></li>
+          </ul>
+          <ul class="rs-list">
+            <li v-for="item in this.$store.state.options.menu.rs"><NuxtLink :to="item.value"><i :class=" item.label "/></NuxtLink></li>
+          </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script>
   export default {
     data() {
+
       return {
-        header : this.$store.state.header,
+        bar1: [],
+        bar2: [],
+        bar3: [],
+        MenuIsActive: false
       }
     },
     methods: {
-      scrollToTop() { window.scroll({ top: 0, left: 0, behavior: 'smooth' }) },
+        scrollToTop() { window.scroll({ top: 0, left: 0, behavior: 'smooth' }) },
+        openMenu: function (event) {
+          this.MenuIsActive = !this.MenuIsActive;
+        },
+
+        equalizer(){
+            const randomNumber = Math.floor(Math.random() * 50 + 20);
+            const marg = (170 - randomNumber) / 2;
+            return 'height:'+ randomNumber +'px;margin-bottom:'+ marg +'px; overflow:hidden;'
+        },
+        updateBars1() {
+          this.bar1 = this.equalizer();
+        },
+        updateBars2() {
+          this.bar2 = this.equalizer();
+        },
+        updateBars3() {
+          this.bar3 = this.equalizer();
+        },
+        getDelay() {
+          return Math.floor(Math.random() * 50 + 20) * 7.5;
+        }
+
+
     },
     computed: {
-      
+
+    },
+    mounted() {
+        this.updateBars1();
+        this.updateBars2();
+        this.updateBars3();
+        setInterval(this.updateBars1, this.getDelay());
+        setInterval(this.updateBars2, this.getDelay());
+        setInterval(this.updateBars3, this.getDelay());
     }
   }
 </script>
