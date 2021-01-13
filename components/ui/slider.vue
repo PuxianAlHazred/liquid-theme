5ba7abcf779d487fec0d44c6dfe72bc2d6cca1b3
Puxian <yoann@piks-l.com>
2021-01-13 11:34:53 +0100

    qsdqsd

--- components/ui/slider.vue
+++ components/ui/slider.vue
@@ -1,20 +1,35 @@
 <template>
-  <div class="swiper"
-    v-swiper:myDirectiveSwiper="this.$store.state.options.plugins.swiperOptions"
-    @ready="onSwiperRedied"
-    @click-slide="onSwiperClickSlide"
-    @slide-change-transition-start="onSwiperSlideChangeTransitionStart"
-  >
-    <div class="swiper-wrapper" >
-      <div class="swiper-slide" v-for="(e, i) in this.$store.state.agenda.posts" :key="i">
-        <NuxtLink :to="e._path+'/'">
-          <span>{{ e.title }}</span>
-          <p>{{ e.date}}</p>
-        </NuxtLink>
+  <div id="slider">
+    <div class="swiper"
+      v-swiper:myDirectiveSwiper="this.$store.state.options.plugins.swiper" @ready="onSwiperRedied" @click-slide="onSwiperClickSlide" @slide-change-transition-start="onSwiperSlideChangeTransitionStart">
+      <div class="swiper-wrapper" >
+        <div class="swiper-slide" v-for="(e, i) in this.$store.state.agenda.posts" :key="i" :lazy-background="e.thumbnail">
+          <NuxtLink :to="e._path+'/'">
+            <span>{{ e.title }}</span>
+            <p>{{ e.date}}</p>
+          </NuxtLink>
+        </div>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        <div class="swiper-pagination" >
+          <div v-for="(e, i) in this.$store.state.agenda.posts" :key="i" >
+            <span>{{ e.date }}</span>
+          </div>
+        </div>
       </div>
+
     </div>
-    <div class="swiper-wrapper-tabs" >
-      <div class="swiper-slide-tabs" v-for="(element, index) in posts" :key="index" :lazy-background="element.thumbnail"></div>
+    <div class="swiper"
+      v-swiper:myDirectiveSwiper="this.$store.state.options.plugins.swiper"
+      @ready="onSwiperRedied"
+      @click-slide="onSwiperClickSlide"
+      @slide-change-transition-start="onSwiperSlideChangeTransitionStart">
+      <div class="swiper-wrapper" >
+        <div class="swiper-slide" v-for="(e, i) in this.$store.state.agenda.posts" :key="i" :lazy-background="e.thumbnail">
+            <p>{{ e.date}}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
