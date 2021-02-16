c954e5cfbcbd9c924e1540e70b8376d4b65ddaa2
Puxian <yoann@piks-l.com>
2021-02-16 12:58:13 +0100

    qsdqsdqd

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -3,12 +3,28 @@
       <section class="content-slug agenda">
         <div class="agenda-header">
           <h3>BLOG</h3>
+
         </div>
       </section>
   </div>
 </template>
 <script>
 export default {
+  head: {
+    htmlAttrs: {
+      lang: 'fr',
+    },
+    title: "Blog - My Website",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Welcome to my website."
+      }
+    ]
+  },
   layout: 'blog',
   data() {
     return {
